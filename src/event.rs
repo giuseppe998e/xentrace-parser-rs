cd77bb407036f7e891f25813aa3bb32348d7986b
Giuseppe Eletto <peppe.eletto@gmail.com>
2021-05-29 01:30:38 +0200

    Update 'event.rs'

--- src/event.rs
+++ src/event.rs
@@ -3,14 +3,15 @@ use std::cmp::Ordering;
 #[derive(Clone, Eq, Debug)]
 pub struct Event {
     code: u32,
-    n_extra: u8,
     in_tsc: bool,
     tsc: u64,
+    n_extra: u8, // 0..7
     extra: [u32; 7],
 }
 
 impl Event {
-    pub fn new(code: u32, tsc: u64, extra: [u32; 7]) -> Self {
+    // CRATE FNs
+    pub(crate) fn new(code: u32) -> Self {
         Self {
             code,
             n_extra: extra.len() as u8,
@@ -20,6 +21,17 @@ impl Event {
         }
     }
 
+    pub(crate) set_extra(&self, extra: [u32; 7]) {
+        self.n_extra = extra.len() as u8;
+        self.extra = extra;
+    }
+
+    pub(crate) fn set_tsc(&self, value: u64) {
+        self.in_tsc = true;
+        self.tsc = value;
+    }
+
+    // PUBLIC FNs
     pub fn get_code(&self) -> u32 {
         self.code
     }
@@ -30,11 +42,6 @@ impl Event {
 
     pub fn get_extra(&self) -> [u32; 7] {
         self.extra.clone()
-    }
-
-    pub(crate) fn set_tsc(&self, value: u64) {
-        self.in_tsc = true;
-        self.tsc = value;
     }
 
     pub fn get_tsc(&self) -> Option<u64> {
