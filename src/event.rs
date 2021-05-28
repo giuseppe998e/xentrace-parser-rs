9f37e5a1a65e69fdb465a930ebe7eecfbde8f4e7
Giuseppe Eletto <peppe.eletto@gmail.com>
2021-05-29 01:24:59 +0200

    Update 'event.rs'

--- src/event.rs
+++ src/event.rs
@@ -1,27 +1,40 @@
-#[derive(Clone, Eq, PartialOrd, Ord, Debug)]
+use std::cmp::Ordering;
+
+#[derive(Clone, Eq, Debug)]
 pub struct Event {
     code: u32,
+    n_extra: u8,
     in_tsc: bool,
     tsc: u64,
     extra: [u32; 7],
 }
 
 impl Event {
-    pub fn new(code: u32, tsc: u64) -> Self {
+    pub fn new(code: u32, tsc: u64, extra: [u32; 7]) -> Self {
         Self {
             code,
+            n_extra: extra.len() as u8,
             in_tsc: tsc > 0,
             tsc,
-            extra: [0; 7],
+            extra,
         }
     }
 
-    pub fn set_extra(&mut self, extra: &[u32; 7]) {
-        self.extra = extra.clone();
+    pub fn get_code(&self) -> u32 {
+        self.code
     }
 
-    pub fn get_extra(&self, pos: u8) -> Option<u32> {
-        self.extra[pos].copy()
+    pub fn get_extra_size(&self) -> u8 {
+        self.n_extra
+    }
+
+    pub fn get_extra(&self) -> [u32; 7] {
+        self.extra.clone()
+    }
+
+    pub(crate) fn set_tsc(&self, value: u64) {
+        self.in_tsc = true;
+        self.tsc = value;
     }
 
     pub fn get_tsc(&self) -> Option<u64> {
@@ -42,5 +55,17 @@ impl PartialEq for Event {
                 .iter()
                 .zip(other.extra.iter())
                 .all(|(a, b)| a == b)
+    }
+}
+
+impl Ord for Event {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.tsc.cmp(&other.tsc)
+    }
+}
+
+impl PartialOrd for Event {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
