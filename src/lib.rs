b1963006f95f0643862a159f15d8577cb68da4ce
Giuseppe Eletto <peppe.eletto@gmail.com>
2021-05-29 23:14:36 +0200

    First working code

--- src/lib.rs
+++ src/lib.rs
@@ -12,11 +12,10 @@ const TRC_SCHED_MIN: u32 = 0x00021000;
 pub struct XTParser {
     // Host CPUs fiels
     cpu_current: u8,
-    cpu_highter: u8,
     cpu_domains: HashMap<u8, Domain>,
-    // Events fiels
+    // Records fiels
     tsc_last: u64,
-    events: Vec<Record>,
+    records: Vec<Record>,
 }
 
 impl XTParser {
@@ -25,57 +24,51 @@ impl XTParser {
         let mut instance = Self {
             // Host CPUs fiels
             cpu_current: 0,
-            cpu_highter: 0,
             cpu_domains: HashMap::new(),
-            // Events fiels
+            // Records fiels
             tsc_last: 0,
-            events: Vec::new(),
+            records: Vec::new(),
         };
 
         let _ = instance.read_file(path)?;
         Ok(instance)
     }
 
-    pub fn get_events(&self) -> &Vec<Record> {
-        &self.events
+    pub fn get_records(&self) -> &Vec<Record> {
+        &self.records
+    }
+
+    pub fn get_records_count(&self) -> usize {
+        self.records.len()
     }
 
     pub fn get_cpu_count(&self) -> u8 {
-        self.cpu_current + 1
+        let cpu_max = self.cpu_domains.keys().max().unwrap();
+        cpu_max + 1
     }
 
     // PRIVATE FNs
     fn read_file(&mut self, path: &str) -> Result<()> {
-        let mut file = File::open(path)?;
+        {
+            let mut file = File::open(path)?;
 
-        loop {
-            match self.read_record(&mut file) {
-                Ok(val) => self.events.push(val),
-                Err(_) => break,
+            loop {
+                let record = self.read_record(&mut file);
+                match record {
+                    Ok(r) => self.records.push(r),
+                    Err(_) => break,
+                }
             }
-        }
+        } // File closed
 
-        self.events.sort();
+        self.records.sort_unstable();
         Ok(())
-    }
-
-    fn update_cpu(&mut self, cpu: u8) {
-        self.cpu_current = cpu;
-
-        if self.cpu_highter < cpu {
-            self.cpu_highter = cpu;
-        }
-    }
-
-    fn update_cpu_dom(&mut self, cpu: u8, dom_u32: u32) -> Option<Domain> {
-        let dom = Domain::from_u32(dom_u32);
-        self.cpu_domains.insert(cpu, dom)
     }
 
     fn read_u32(file: &mut File) -> Result<u32> {
         let mut buf = [0u8; 4];
-        file.read(&mut buf)?;
-        Ok(u32::from_be_bytes(buf))
+        file.read_exact(&mut buf)?;
+        Ok(u32::from_ne_bytes(buf)) // host-endian because of XenTrace
     }
 
     fn read_tsc(hdr: u32, file: &mut File) -> Result<Option<u64>> {
@@ -84,15 +77,15 @@ impl XTParser {
             false => Ok(None),
             true => {
                 let mut buf = [0u8; 8];
-                file.read(&mut buf)?;
-                Ok(Some(u64::from_be_bytes(buf)))
+                file.read_exact(&mut buf)?;
+                Ok(Some(u64::from_ne_bytes(buf))) // host-endian because of XenTrace
             }
         }
     }
 
     fn read_extra(hdr: u32, file: &mut File) -> Result<Vec<u32>> {
-        let n_extra = (hdr >> 28) & 0x7;
-        let mut extra: Vec<u32> = vec![];
+        let n_extra = (hdr >> 28) & 7;
+        let mut extra: Vec<u32> = Vec::new();
 
         for _ in 0..n_extra {
             let value = Self::read_u32(file)?;
@@ -103,40 +96,47 @@ impl XTParser {
     }
 
     fn read_record(&mut self, file: &mut File) -> Result<Record> {
+        // Read header
         let hdr = Self::read_u32(file)?;
 
+        // Read event data
         let code = hdr & 0x0fffffff;
         let tsc = Self::read_tsc(hdr, file)?;
         let extra = Self::read_extra(hdr, file)?;
 
-        // Check TRC_TRACE_CPU_CHANGE event
-        if code == TRC_TRACE_CPU_CHANGE {
-            let cpu = (*extra.get(0).unwrap()) as u8;
-            self.update_cpu(cpu);
-        } else {
-            // Check TRC_SCHED_MIN family
-            let is_sched_min = code == (code & (TRC_SCHED_MIN | 0xf0f));
-            if is_sched_min {
-                self.update_cpu_dom(self.cpu_current, *extra.get(0).unwrap());
+        // Create event
+        let mut event = Event::new(code);
+        event.set_extra(extra.as_slice());
+        match tsc {
+            None => event.set_tsc(self.tsc_last),
+            Some(v) => {
+                self.tsc_last = v;
+                event.set_tsc(v);
             }
         }
 
-        let mut event = Event::new(code);
-        event.set_extra(extra.as_slice());
-        if let Some(val) = tsc {
-            self.tsc_last = val;
-            event.set_tsc(val);
+        // Handle special events
+        if code == TRC_TRACE_CPU_CHANGE {
+            let cpu = *extra.get(0).unwrap() as u8;
+            self.cpu_current = cpu;
         } else {
-            event.set_tsc(self.tsc_last);
+            let is_sched_min = code == (code & (TRC_SCHED_MIN | 0xf0f));
+            if is_sched_min {
+                let dom_u32 = *extra.get(0).unwrap();
+                let dom = Domain::from_u32(dom_u32);
+                self.cpu_domains.insert(self.cpu_current, dom);
+            }
         }
 
-        let dom_current = self.cpu_domains.get(&self.cpu_current);
-        let dom_current = match dom_current {
-            Some(&v) => v,
+        // Get current domain
+        let domain = self.cpu_domains.get(&self.cpu_current);
+        let domain = match domain {
             None => Domain::default(),
+            Some(&v) => v,
         };
 
-        let record = Record::new(self.cpu_current, dom_current, event);
+        // Create record
+        let record = Record::new(self.cpu_current, domain, event);
         Ok(record)
     }
 }
@@ -147,11 +147,15 @@ mod tests {
 
     #[test]
     fn parser_print() {
-        let parser = XTParser::new("/home/giuseppe/Downloads/trace_xen.bin").unwrap();
-        let records = parser.get_events();
+        let code = XTParser::new("/home/giuseppe/Downloads/trace_xen.bin").unwrap();
+        let records = code.get_records();
 
         for rec in records {
             println!("{:?}", rec);
         }
+
+        println!(); // BLANK
+        println!("Records count: {:?}", code.get_records_count());
+        println!("CPU count: {:?}", code.get_cpu_count());
     }
 }
