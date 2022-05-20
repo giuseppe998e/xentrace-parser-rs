46e7654eb179b2db692611312ef317399e82cf7d
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-05-20 11:17:10 +0200

    Minor changes

--- src/lib.rs
+++ src/lib.rs
@@ -43,12 +43,14 @@ pub fn xentrace_parse(path: &str) -> Result<Trace> {
     Ok(trace)
 }
 
+#[inline]
 fn read_u32(file: &mut File) -> Result<u32> {
     let mut buf = [0u8; 4];
     file.read_exact(&mut buf)?;
     Ok(u32::from_ne_bytes(buf)) // host-endian because of XenTrace
 }
 
+#[inline(always)]
 fn read_u64(file: &mut File) -> Result<u64> {
     let mut buf = [0u8; 8];
     file.read_exact(&mut buf)?;
