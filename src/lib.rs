3019a74fae87ec0cfcdcca77e48fbd9857a265e5
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-07-11 16:46:34 +0200

    Update library

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::{
     fs::File,
     io::{Error, ErrorKind, Result},
     path::Path,
+    rc::Rc,
 };
 
 pub mod codes;
@@ -15,7 +16,7 @@ mod trace;
 pub use trace::Trace;
 
 mod util;
-use util::{read_u32, read_u64};
+use util::fs::ReadNumber;
 
 /// Returns the [Trace](trace::Trace) structure, enclosed in an [std::io:Result](std::io::Result),
 /// containing the XenTrace binary file records parsed.
@@ -42,7 +43,7 @@ pub fn xentrace_parse(file: &str) -> Result<Trace> {
 
         let mut last_tsc = 0u64;
         let mut current_cpu = 0u16;
-        let mut cpus_dom = HashMap::<u16, Domain>::new();
+        let mut cpus_dom = HashMap::<u16, Rc<Domain>>::with_capacity(32);
 
         loop {
             let record = parse_record(&mut file, &mut last_tsc, &mut current_cpu, &mut cpus_dom);
@@ -64,7 +65,7 @@ pub fn xentrace_parse(file: &str) -> Result<Trace> {
 }
 
 fn parse_event(file: &mut File, last_tsc: &mut u64) -> Result<Event> {
-    let hdr = read_u32(file)?;
+    let hdr = file.read_u32()?;
 
     // Code
     let code = hdr & 0x0FFFFFFF;
@@ -74,7 +75,7 @@ fn parse_event(file: &mut File, last_tsc: &mut u64) -> Result<Event> {
     let tsc = {
         let in_tsc = (hdr & (1 << 31)) > 0;
         if in_tsc {
-            *last_tsc = read_u64(file)?;
+            *last_tsc = file.read_u64()?;
         }
 
         *last_tsc
@@ -87,7 +88,7 @@ fn parse_event(file: &mut File, last_tsc: &mut u64) -> Result<Event> {
 
         if n_extra > 0 {
             for e in extra.iter_mut().take(n_extra) {
-                *e = Some(read_u32(file)?);
+                *e = file.read_u32().map(Some)?;
             }
         }
 
@@ -101,7 +102,7 @@ fn parse_record(
     file: &mut File,
     last_tsc: &mut u64,
     current_cpu: &mut u16,
-    cpus_dom: &mut HashMap<u16, Domain>,
+    cpus_dom: &mut HashMap<u16, Rc<Domain>>,
 ) -> Result<Record> {
     let event = parse_event(file, last_tsc)?;
     let code = u32::from(event.code);
@@ -111,14 +112,13 @@ fn parse_record(
         return Err(Error::from(ErrorKind::Other)); // Do not save this kind of events
     }
 
-    let domain = match code == (code & TRC_SCHED_TO_RUN) {
-        true => {
-            let extra_0 = event.extra[0].unwrap_or(0);
-            let dom = Domain::from(extra_0);
-            cpus_dom.insert(*current_cpu, dom);
-            Some(dom)
-        }
-        false => cpus_dom.get(current_cpu).copied(),
+    let domain = if code == (code & TRC_SCHED_TO_RUN) {
+        let extra_0 = event.extra[0].unwrap_or(0);
+        let dom = Rc::new(Domain::from(extra_0));
+        cpus_dom.insert(*current_cpu, Rc::clone(&dom));
+        Some(dom)
+    } else {
+        cpus_dom.get(current_cpu).map(Rc::clone)
     }
     .unwrap_or_default();
 
