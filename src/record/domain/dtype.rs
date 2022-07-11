ef7d82cfd977a908aa8fbda5825e99a048894514
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-07-11 16:45:53 +0200

    Update Domain sub-module

--- src/record/domain/dtype.rs
+++ src/record/domain/dtype.rs
@@ -1,24 +1,18 @@
-#[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
-
 /// Type of virtual machine.
-pub enum DomainType {
-    /// The zero/host domain (*The one that has access to hardware*).
+#[derive(Clone, Copy, Default, Debug, Eq, Ord, PartialEq, PartialOrd)]
+pub enum DomainKind {
+    /// The zero/host domain (*The privileged VM*).
     Zero,
-    /// The idle domain (*The host CPU is not used*).
+    /// The idle domain (*The CPU is not used*).
     Idle,
-    /// The default domain (*No information is available*).
+    /// The default domain (*No info available*).
+    #[default]
     Default,
-    /// The unprivileged domain (*The "real" running VMs*).
+    /// The unprivileged domain (*The running VMs*).
     Guest(u16),
 }
 
-impl Default for DomainType {
-    fn default() -> Self {
-        DomainType::Default
-    }
-}
-
-impl From<u16> for DomainType {
+impl From<u16> for DomainKind {
     fn from(val: u16) -> Self {
         match val {
             0 => Self::Zero,
@@ -29,90 +23,90 @@ impl From<u16> for DomainType {
     }
 }
 
-impl From<DomainType> for u16 {
-    fn from(val: DomainType) -> Self {
+impl From<DomainKind> for u16 {
+    fn from(val: DomainKind) -> Self {
         match val {
-            DomainType::Zero => 0,
-            DomainType::Idle => 32767,
-            DomainType::Default => 32768,
-            DomainType::Guest(v) => v,
+            DomainKind::Zero => 0,
+            DomainKind::Idle => 32767,
+            DomainKind::Default => 32768,
+            DomainKind::Guest(v) => v,
         }
     }
 }
 
-impl From<DomainType> for u32 {
-    fn from(val: DomainType) -> Self {
+impl From<DomainKind> for u32 {
+    fn from(val: DomainKind) -> Self {
         u32::from(u16::from(val))
     }
 }
 
-impl From<DomainType> for i32 {
-    fn from(val: DomainType) -> Self {
+impl From<DomainKind> for i32 {
+    fn from(val: DomainKind) -> Self {
         i32::from(u16::from(val))
     }
 }
 
-impl From<DomainType> for u64 {
-    fn from(val: DomainType) -> Self {
+impl From<DomainKind> for u64 {
+    fn from(val: DomainKind) -> Self {
         u64::from(u16::from(val))
     }
 }
 
-impl From<DomainType> for i64 {
-    fn from(val: DomainType) -> Self {
+impl From<DomainKind> for i64 {
+    fn from(val: DomainKind) -> Self {
         i64::from(u16::from(val))
     }
 }
 
-impl From<DomainType> for u128 {
-    fn from(val: DomainType) -> Self {
+impl From<DomainKind> for u128 {
+    fn from(val: DomainKind) -> Self {
         u128::from(u16::from(val))
     }
 }
 
-impl From<DomainType> for i128 {
-    fn from(val: DomainType) -> Self {
+impl From<DomainKind> for i128 {
+    fn from(val: DomainKind) -> Self {
         i128::from(u16::from(val))
     }
 }
 
-impl From<DomainType> for usize {
-    fn from(val: DomainType) -> Self {
+impl From<DomainKind> for usize {
+    fn from(val: DomainKind) -> Self {
         usize::from(u16::from(val))
     }
 }
 
-impl TryFrom<DomainType> for isize {
+impl TryFrom<DomainKind> for isize {
     type Error = std::num::TryFromIntError;
 
-    fn try_from(value: DomainType) -> Result<Self, Self::Error> {
+    fn try_from(value: DomainKind) -> Result<Self, Self::Error> {
         isize::try_from(u16::from(value))
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::DomainType;
+    use super::DomainKind;
 
     #[test]
     fn into_u16_test() {
-        let type_ = DomainType::Guest(12345);
+        let type_ = DomainKind::Guest(12345);
 
         assert_eq!(u16::from(type_), 12345);
     }
 
     #[test]
     fn equality_test() {
-        let type1 = DomainType::from(55u16);
-        let type2 = DomainType::from(55);
+        let type1 = DomainKind::from(55u16);
+        let type2 = DomainKind::from(55);
 
         assert_eq!(type1, type2);
     }
 
     #[test]
     fn not_equality_test() {
-        let type1 = DomainType::from(0);
-        let type2 = DomainType::default();
+        let type1 = DomainKind::from(0);
+        let type2 = DomainKind::default();
 
         assert_ne!(u128::from(type1), type2.into());
     }
